9417500f04a9e97a0ef45fcd3bb5af37b6080e2e
Pratiksha <pratikshapatel1182@gmail.com>
2020-02-08 16:21:01 +0000

    hi

---
+++ src/printpractice/BioData.java
@@ -0,0 +1,11 @@
+package printpractice;
+
+public class BioData {
+    public static void main(String[] args){
+        System.out.println("Name= Pratiksha");
+        System.out.println("Surname= Patel");
+        System.out.println("Address= Hatfield");
+    }
+
+
+}
